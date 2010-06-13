824439c0454e8ca3c774c19612422363d7c27ab5
Nat Welch <nat@natwelch.com>
2010-06-13 14:31:47 -0700

    Moving things into a doc folder

--- website.rb
+++ website.rb
@@ -17,37 +17,37 @@ end
 
 get '/proposal' do
    title = "Senior Project Proposal"
-   md = RDiscount.new(File.read("proposal.md"), :smart).to_html
+   md = RDiscount.new(File.read("docs/proposal.md"), :smart).to_html
    erb :page, :locals => { :title => title, :content => md }
 end
 
 get '/schedule' do
    title = "Senior Project Schedule"
-   content = "<pre>\n#{File.read("schedule.txt")}\n</pre>"
+   content = "<pre>\n#{File.read("docs/schedule.txt")}\n</pre>"
    erb :page, :locals => { :title => title, :content => content }
 end
 
 get '/notes' do
    title = "Senior Project Notes"
-   md = RDiscount.new(File.read("notes.md"), :smart).to_html
+   md = RDiscount.new(File.read("docs/notes.md"), :smart).to_html
    erb :page, :locals => { :title => title, :content => md }
 end
 
 get '/spec' do
    title = "Senior Project Specification"
-   md = RDiscount.new(File.read("spec.md"), :smart).to_html
+   md = RDiscount.new(File.read("docs/spec.md"), :smart).to_html
    erb :page, :locals => { :title => title, :content => md }
 end
 
 get '/ui' do
    title = "Senior Project UI Prototype"
-   md = RDiscount.new(File.read("ui_prototype.md"), :smart).to_html
+   md = RDiscount.new(File.read("docs/ui_prototype.md"), :smart).to_html
    erb :page, :locals => { :title => title, :content => md }
 end
 
 get '/survey' do
    title = "Senior Project Questionnaire"
-   md = RDiscount.new(File.read("Questionaire.md"), :smart).to_html
+   md = RDiscount.new(File.read("docs/Questionaire.md"), :smart).to_html
    erb :page, :locals => { :title => title, :content => md }
 end
 
