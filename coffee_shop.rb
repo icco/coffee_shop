0bc32fd744fa1c6a84c73b1d13aac3f00edf2109
Nat Welch <nat@natwelch.com>
2010-06-30 23:55:12 -0700

    qt?

--- coffee_shop.rb
+++ coffee_shop.rb
@@ -1,59 +1,17 @@
 #!/usr/bin/env ruby
 
-#Shoes.app {
-#   button("Turl?") {
-#      alert("DUDE!")
-#   }
-#}
+require 'Qt4'
 
-#Shoes.app do
-#   para "testing", " no seriously... I'm testing...."
-#   para "Oh, to fling and be flung.", :size => 48
-#end
+# http://www.darshancomputing.com/qt4-qtruby-tutorial/chapter_03
+app = Qt::Application.new(ARGV)
 
-#Shoes.app do
-#  @o = oval :top => 0, :left => 0,
-#             :radius => 40
-#  stack :margin => 40 do
-#    title "Dancing With a Circle"
-#    subtitle "How graceful and round."
-#  end
-#  motion do |x, y|
-#    @o.move width - x, height - y
-#  end
-#end
+quit = Qt::PushButton.new('Quit')
+quit.resize(75, 30)
+quit.setFont(Qt::Font.new('Times', 18, Qt::Font::Bold))
 
-#Shoes.app do
-#  @e = edit_line :width => 400
-#  button “O.K.” do
-#    alert @e.text
-#  end
-#end
+Qt::Object.connect(quit, SIGNAL('clicked()'), app, SLOT('quit()'))
 
-#Shoes.app do
-#  @e = edit_box "Would that I...",
-#    :width => 400, :height => 240
-#end
+quit.show()
+app.exec()
 
-class BookList < Shoes
-  url '/',      :index
-  url '/twain', :twain
-  url '/kv',    :vonnegut
-  def index
-    para "Books I’ve read: ",
-      link("by Mark Twain", :click => "/twain"),
-      link(" by Kurt Vonnegut", :click => "/kv")
-  end
-  def twain
-    para "Just Huck Finn.\n",
-      link("Go back.", :click => "/")
-  end
-  def vonnegut
-    para "Cat’s Cradle. Sirens of Titan. ",
-      "Breakfast of Champions.\n",
-      link("Go back.", :click => "/")
-  end
-end
-
-Shoes.app :width => 400, :height => 500
 
