ca7fa9ad245449092d719d79cb1f41f1819b962e
Nat Welch <nat@natwelch.com>
2010-07-03 23:43:02 -0700

    Playing some more with Qt. Looks like what I want.

--- coffee_shop.rb
+++ coffee_shop.rb
@@ -5,13 +5,14 @@ require 'Qt4'
 # http://www.darshancomputing.com/qt4-qtruby-tutorial/chapter_03
 app = Qt::Application.new(ARGV)
 
-quit = Qt::PushButton.new('Quit')
-quit.resize(75, 30)
-quit.setFont(Qt::Font.new('Times', 18, Qt::Font::Bold))
+window = Qt::Widget.new() # Root Window
+window.setWindowState(Qt::WindowFullScreen)
 
+quit = Qt::PushButton.new('Quit', window)
+quit.font = Qt::Font.new('Times', 18, Qt::Font::Bold)
+quit.setGeometry(10, 40, 180, 40)
 Qt::Object.connect(quit, SIGNAL('clicked()'), app, SLOT('quit()'))
 
-quit.show()
+window.show()
 app.exec()
-
 
